6f54ee46ccc9ee7f87d0f77a2902ea4a93b56a3d
Shad0wHunt3r <khaienkoh@gmail.com>
2026-05-09 16:47:17 +0800

    initial project setup

---
+++ src/client/java/com/shad0whunt3r/maceutils/client/MaceUtilsHUDClient.java
@@ -0,0 +1,10 @@
+package com.shad0whunt3r.maceutils.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaceUtilsHUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
