072148e0f63a0da1fcb923673778ff22e3cef17e
Shad0wHunt3r <khaienkoh@gmail.com>
2026-05-09 17:06:09 +0800

    added library import code to ModConfig.java

---
+++ src/client/java/com/shad0whunt3r/maceutils/client/ModConfig.java
@@ -0,0 +1,14 @@
+package com.shad0whunt3r.maceutils.client;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ModConfig {
+
+}
