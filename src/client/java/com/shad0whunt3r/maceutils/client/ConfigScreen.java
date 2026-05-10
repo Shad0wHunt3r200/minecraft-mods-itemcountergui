e0da1524f7178216f3dc528fc6f38bec5521ead8
Shad0wHunt3r <khaienkoh@gmail.com>
2026-05-10 17:57:21 +0800

    finished ConfigScreen

--- src/client/java/com/shad0whunt3r/maceutils/client/ConfigScreen.java
+++ src/client/java/com/shad0whunt3r/maceutils/client/ConfigScreen.java
@@ -1,8 +1,15 @@
 package com.shad0whunt3r.maceutils.client;
 
+import dev.isxander.yacl3.api.ConfigCategory;
+import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.YetAnotherConfigLib;
+import dev.isxander.yacl3.api.controller.ColorControllerBuilder;
+import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+
+import java.awt.*;
+import java.util.List;
 
 public class ConfigScreen extends Screen {
     private final Screen parent;
@@ -14,7 +21,30 @@ public class ConfigScreen extends Screen {
 
     @Override
     protected void init() {
-        var config = YetAnotherConfigLib.createBuilder().title(Component.literal("Mace Utils"));
+        var enabledToggle = Option.<Boolean>createBuilder()
+                .name(Component.literal("Enable/Disable Mod"))
+                .binding(true, () -> ModConfig.INSTANCE.enabled, val -> ModConfig.INSTANCE.enabled = val)
+                .controller(TickBoxControllerBuilder::create)
+                .build();
 
+        var colorSelector = Option.<Color>createBuilder()
+                .name(Component.literal("Text Color"))
+                .binding(Color.white, () -> new Color(ModConfig.INSTANCE.textColor), val -> ModConfig.INSTANCE.textColor = val.getRGB())
+                .controller(ColorControllerBuilder::create)
+                .build();
+
+        var configCategory = ConfigCategory.createBuilder()
+                .name(Component.literal("General"))
+                .option(enabledToggle)
+                .option(colorSelector)
+                .build();
+
+        var config = YetAnotherConfigLib.createBuilder()
+                .title(Component.literal("Mace Utils"))
+                .categories(List.of(configCategory))
+                .save(ModConfig::save)
+                .build();
+
+        this.minecraft.setScreen(config.generateScreen(parent));
     }
 }
