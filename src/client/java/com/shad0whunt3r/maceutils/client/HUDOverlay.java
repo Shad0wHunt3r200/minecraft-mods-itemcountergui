58a44a758d94d3625b19b11714c756a4995d27f7
Shad0wHunt3r <khaienkoh@gmail.com>
2026-05-12 16:29:49 +0800

    Added HUDOverlay code

---
+++ src/client/java/com/shad0whunt3r/maceutils/client/HUDOverlay.java
@@ -0,0 +1,35 @@
+package com.shad0whunt3r.maceutils.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class HUDOverlay {
+    public static void render(GuiGraphics graphics, float tickDelta) {
+        if (!ModConfig.INSTANCE.enabled) return;
+
+        int x = 5;
+        int y = 5;
+
+        drawItem(graphics, Items.WIND_CHARGE, ItemCounter.countItems(Items.WIND_CHARGE), x, y);
+        y += 20;
+
+        drawItem(graphics, Items.ENDER_PEARL, ItemCounter.countItems(Items.ENDER_PEARL), x, y);
+        y += 20;
+
+        drawItem(graphics, Items.ELYTRA, ItemCounter.getDurability(Items.ELYTRA), x, y);
+        y += 20;
+    }
+
+    private static void drawItem(GuiGraphics graphics, Item item, int count, int x, int y) {
+        if (count == 0) return;
+
+        var itemStack = new ItemStack(item);
+
+        graphics.renderItem(itemStack, x, y);
+        graphics.drawString(Minecraft.getInstance().font, String.valueOf(count), x + 18, y + 5, ModConfig.INSTANCE.textColor);
+    }
+}
+
